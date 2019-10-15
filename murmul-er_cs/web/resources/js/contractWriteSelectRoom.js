a5f0d386d213723809e47db0a1d5ba5c3f19a7ec
ine <enchanted13th@gmail.com>
2019-10-15 18:39:50 +0900

    ine

--- murmul-er_cs/web/resources/js/contractWriteSelectRoom.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    $('.selectRoom').click(function () {
-        let roomId = $(this).val();
-        location.href = "/contract/write?contactId="+contactId+"&roomId="+roomId;
-    })
-})
